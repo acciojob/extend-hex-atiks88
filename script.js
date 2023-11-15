318afa0fc9f7c2843db0d64b8f254edf1fc9614d
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-15 18:58:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  shortHex = shortHex.replace("#", "");
+
+  if (shortHex.length !== 3 || !/^[0-9a-fA-F]{3}$/.test(shortHex)) {
+    console.error("Invalid short hex code");
+    return null;
+  }
+
+  const fullHex = shortHex
+    .split("")
+    .map(component => component.repeat(2))
+    .join("");
+
+  return `#${fullHex}`;
 };
 
-// Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
